81f56efda98f856b00b6c2aa3187935cef32294e
Taylor Sather <fivetentaylor@gmail.com>
2015-07-19 15:29:59 -0700

    Adding the initial bits

---
+++ README.md
@@ -0,0 +1,3 @@
+#PyDeploy
+
+Deploy groups of command line tools under organized namespaces
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyDeploy',
+      version='0.0.1',
+      description='Deploy Command Lines Tools with namespaces',
+      author='Taylor Sather',
+      author_email='fivetentaylor@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['pydeploy'],
+     )
